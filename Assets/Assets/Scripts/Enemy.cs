7ff37181302a1f382d97b9ead2946fe41d74d4f4
OffensivelyBad <OffensivelyBad@yahoo.com>
2018-03-19 20:39:38 -0400

    adds scripts and scenes

---
+++ Assets/Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] int target = 0;
+    [SerializeField] Transform finish = null;
+    [SerializeField] Transform[] checkPoints = null;
+    [SerializeField] float navigationUpdate = 0f;
+
+    private Transform enemyPosition;
+    private float navigationTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+        enemyPosition = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (checkPoints != null) {
+            navigationTime += Time.deltaTime;
+            if (navigationTime > navigationUpdate) {
+                if (target < checkPoints.Length) {
+                    enemyPosition.position = Vector2.MoveTowards(enemyPosition.position, checkPoints[target].position, navigationTime);
+                }
+                else {
+                    enemyPosition.position = Vector2.MoveTowards(enemyPosition.position, checkPoints[target].position, navigationTime);
+                }
+                navigationTime = 0;
+            }
+        }
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        if (collision.tag == "Checkpoint")
+            target += 1;
+        else if (collision.tag == "Finish") {
+            GameManager.instance.removeEnemy();
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ Assets/Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    [SerializeField] GameObject spawnPoint = null;
+    [SerializeField] GameObject[] enemies = null;
+    [SerializeField] int maxEnemiesOnScreen = 1;
+    [SerializeField] int totalEnemies = 1;
+    [SerializeField] int enemiesPerSpawn = 1;
+    [SerializeField] float spawnDelay = 0.5f;
+
+    private int enemiesOnScreen = 0;
+
+	private void Awake()
+	{
+        if (instance == null)
+            instance = this;
+        else if (instance != this) {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(spawn());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator spawn() {
+        if (enemiesPerSpawn > 0 && enemiesOnScreen < totalEnemies)
+        {
+            for (int i = 0; i < enemiesPerSpawn; i++)
+            {
+                if (enemiesOnScreen < maxEnemiesOnScreen)
+                {
+                    GameObject newEnemy = Instantiate(enemies[0]) as GameObject;
+                    newEnemy.transform.position = spawnPoint.transform.position;
+                    enemiesOnScreen += 1;
+                }
+            }
+        }
+        yield return new WaitForSeconds(spawnDelay);
+        StartCoroutine(spawn());
+    }
+
+    public void removeEnemy() {
+        if (enemiesOnScreen > 0) {
+            enemiesOnScreen -= 1;
+        }
+    }
+}
