ca1c07d84f92228a31ad55818b1cfe5fdac3b4db
OffensivelyBad <OffensivelyBad@yahoo.com>
2018-03-20 23:29:31 -0400

    Adds tower buttons

---
+++ Assets/Assets/Scripts/TowerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerManager : Singleton<TowerManager> {
+
+    private TowerBtn towerBtnPressed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void selectedTower(TowerBtn selectedTowerBtn) {
+        towerBtnPressed = selectedTowerBtn;
+        Debug.Log("selected tower: " + towerBtnPressed.gameObject);
+    }
+
+}
